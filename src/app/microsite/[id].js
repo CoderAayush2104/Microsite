92b6fa7c69c1e1baf1c2f14b914465b60623aa1c
Aayush Patel <aayush.2104@gmail.com>
2024-12-24 22:35:29 +0530

    Initial Commit

---
+++ src/app/microsite/[id].js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const MicrositePage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id)
+  const micrositeData = JSON.parse(sessionStorage.getItem('micrositeData'));
+
+  return (
+    <div>
+      <h1>{micrositeData.storeName} Microsite</h1>
+      <p>{micrositeData.storeDescription}</p>
+    </div>
+  );
+};
+
+export default MicrositePage;
